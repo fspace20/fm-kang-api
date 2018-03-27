48e7a1e5b2043003dc5867980e67f944acb83117
fspace20 <devkite@Kites-MacBook-Pro.local>
2018-03-27 22:16:13 +0900

    handler added: parsing & sending msg

--- sched-crawler/app/handler.js
+++ sched-crawler/app/handler.js
@@ -11,6 +11,63 @@ const chatId = bot.getChat(channelId).id;
 
 
 module.exports.crawler = (event, context, callback) => {
+  let result;
+
+  // get date info
+  var today = new Date();
+  var tdYmd = dateToYmd(today);
+  console.log('function "${context.functionName}" starts at ${today}');
+
+  const url = 'http://www.mofa.go.kr/minister/pgm/m_20032/uss/schedule/schedule.do'
+  const FormData = require('form-data');
+  const form = new FormData();
+   
+  form.append('type', 'move');
+  form.append('tdDate', tdYmd);
+  form.append('fstweek', tdYmd);
+   
+  console.log('POST:' + url);
+  got.post(url, {
+      body: form
+  }).then(response => { 
+    result = response.body;
+    console.log(result);
+  }).then(() => {
+    console.log('THEN');
+
+    // get today schedule
+    if(result != undefined){
+      // parse schedule keys
+      var obj = JSON.parse(result);
+      var keys = Object.keys(obj);
+      for(var i = 0; i < keys.length; i++){
+        console.log('KEY:' + keys[i]);
+      }
+
+      // parse today schedule
+      var daySched = '오늘 일정이 없습니다.';
+      var dayScheds = obj[keys[2]];
+      for (var i = 0; i < dayScheds.length; i++) {
+        if(i == 0){
+          daySched = '';
+          console.log('SCHED[0]:' + dayScheds[i]);
+        }
+        daySched += schedToTxt(dayScheds[i]);
+      }
+
+      // send schedule message
+      var title = "[장관일정] " + tdYmd.substr(0,4) 
+                + "-" + tdYmd.substr(4,2) 
+                + "-" + tdYmd.substr(6,2) 
+                + "\n";
+      var msg = title + daySched;
+      bot.sendMessage(channelId, msg);
+    }
+  }).catch(error => {
+    console.log('ERROR:' + error);
+  });
+    
+  // set HTTP response
   const response = {
     statusCode: 200,
     body: JSON.stringify({

619ef99e1d285e56891db5d204b4fadf1b698cfa
fspace20 <devkite@Kites-MacBook-Pro.local>
2018-03-27 22:13:23 +0900

    handler updated: bot setting added

--- sched-crawler/app/handler.js
+++ sched-crawler/app/handler.js
@@ -1,5 +1,15 @@
 'use strict';
 
+const got = require('got');
+const TelegramBot = require('node-telegram-bot-api');
+
+// set telegram bot
+const token = require('./bot.config.json').TOKEN;
+const bot = new TelegramBot(token);
+const channelId = '@fm_kang_bot';
+const chatId = bot.getChat(channelId).id;
+
+
 module.exports.crawler = (event, context, callback) => {
   const response = {
     statusCode: 200,
